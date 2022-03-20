2e7cf82bd65163acbfb258551cc9cc32079effc8
Aiden <47725379+Mewrin@users.noreply.github.com>
2022-03-20 20:24:19 +0000

    draw selected area
    I came up with a very simple formula that enables width and height to be determined by the cursor positon, so currently using that to draw the rectangle. Currently the rectangles are invalidated every time which makes them flicker, so that needs fixing.

--- AiDesktopService.cs
+++ AiDesktopService.cs
@@ -11,6 +11,8 @@ namespace AIDesktop
     {
         private Form _parentForm;
 
+        private SelectableAreaForm _selectableAreaForm;
+
         public AiDesktopService(Form form)
         {
             _parentForm = form;
@@ -18,18 +20,31 @@ namespace AIDesktop
 
         public void CaptureScreen(Screen screenToCapture, string pathToSaveCapturesTo)
         {
+            int bufferTimeForFormToHide = 250;
             _parentForm.Hide();
-            Thread.Sleep(250);
+            Thread.Sleep(bufferTimeForFormToHide);
 
-            var captureBitmap = new Bitmap(screenToCapture.WorkingArea.Width, screenToCapture.WorkingArea.Height, PixelFormat.Format32bppArgb);
-            var captureGraphics = Graphics.FromImage(captureBitmap);
+            var screenCapture = new Bitmap(screenToCapture.WorkingArea.Width, screenToCapture.WorkingArea.Height, PixelFormat.Format32bppArgb);
 
+            var captureGraphics = Graphics.FromImage(screenCapture);
             captureGraphics.CopyFromScreen(screenToCapture.WorkingArea.Left, screenToCapture.WorkingArea.Top, 0, 0, screenToCapture.WorkingArea.Size);
-            captureBitmap.Save(pathToSaveCapturesTo, ImageFormat.Jpeg);
 
-            // TODO: test
+            //captureBitmap.Save(pathToSaveCapturesTo, ImageFormat.Jpeg);
+
+            // create new form, make it transparent with a border, with a button that says 'capture')
+            // -- this can be edited later, but works for now
+
+            
+            _selectableAreaForm = new SelectableAreaForm(screenCapture);
+            _selectableAreaForm.Show();
+
 
             _parentForm.Show();
+        }
+
+        private Bitmap ResizeBitmap(Bitmap image)
+        {
+            return new Bitmap(image, new Size(image.Width / 2, image.Height / 2));
         }
     }
 }
---
+++ SelectableAreaForm.cs
@@ -0,0 +1,84 @@
+﻿namespace AIDesktop
+{
+    public partial class SelectableAreaForm : Form
+    {
+        Bitmap _image;
+
+        int _startX = 0;
+        int _startY = 0;
+        int _lastX = 0;
+        int _lastY = 0;
+        int _width = 0;
+        int _height = 0;
+
+        bool _isMovingMouse;
+
+        public SelectableAreaForm(Bitmap image)
+        {
+            InitializeComponent();
+            _image = image;
+        }
+
+        private void SelectableAreaForm_Load(object sender, EventArgs e)
+        {
+            Width = _image.Width;
+            Height = _image.Height;
+            pictureBoxScreenshot.Width = _image.Width;
+            pictureBoxScreenshot.Height = _image.Height;
+            pictureBoxScreenshot.Image = _image;
+
+            // add event handlers
+            pictureBoxScreenshot.MouseDown += new MouseEventHandler(SelectableAreaForm_MouseDown);
+            pictureBoxScreenshot.MouseUp += new MouseEventHandler(SelectableAreaForm_MouseUp);
+            pictureBoxScreenshot.MouseMove += new MouseEventHandler(SelectableAreaForm_MouseMove);
+        }
+
+        private void SelectableAreaForm_MouseMove(object? sender, MouseEventArgs e)
+        {
+            if (_isMovingMouse)
+            {
+                _width = e.X - _startX;
+                _height = e.Y - _startY;
+
+                _lastX = e.X;
+                _lastY = e.Y;
+                using (var graphics = pictureBoxScreenshot.CreateGraphics())
+                {
+                    pictureBoxScreenshot.Invalidate();
+                    var selectedRectangle = new Rectangle(_startX, _startY, _width, _height);
+                    using (var pen = new Pen(Color.Red, 1))
+                    {
+                        graphics.DrawRectangle(pen, selectedRectangle);
+                    }
+                }
+            }
+        }
+
+        private void SelectableAreaForm_MouseDown(object? sender, MouseEventArgs @event)
+        {
+            _isMovingMouse = true;
+            
+            _startX = @event.X;
+            _startY = @event.Y;
+        }
+
+        private void SelectableAreaForm_MouseUp(object? sender, MouseEventArgs @event)
+        {
+            _isMovingMouse = false;
+
+            using (var graphics = pictureBoxScreenshot.CreateGraphics())
+            {
+                var selectedRectangle = new Rectangle(_startX, _startY, _width, _height);
+                using (var pen = new Pen(Color.Red, 1))
+                {
+                    graphics.DrawRectangle(pen, selectedRectangle);
+                }
+            }
+
+            _lastX = 0;
+            _lastY = 0;
+            _width = 0;
+            _height = 0;
+        }
+    }
+}
